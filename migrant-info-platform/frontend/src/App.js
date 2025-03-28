6f811461de502177c8d62c74c4df649912d42216
Gerdagithub <gerdaliukas98@gmail.com>
2025-03-28 17:22:19 +0200

    Created a container of category cards in the homepage

--- migrant-info-platform/frontend/src/App.js
+++ migrant-info-platform/frontend/src/App.js
@@ -5,12 +5,14 @@ import 'bootstrap/dist/js/bootstrap.min.js'; // sometimes may be needed
 import { WEB_NAME } from './constants.js';
 
 import Navbar from "./components/Navbar/Navbar.jsx"
+import CategoryCardsContainer from "./components/CategoryCardsContainer/CategoryCardsContainer.jsx"
 
 function App() {
   return (
     <>
       <Navbar/>
       <h1 className='text-center mt-5 mb-3'>{WEB_NAME}</h1>
+      <CategoryCardsContainer/>
     </>
   );
 }
